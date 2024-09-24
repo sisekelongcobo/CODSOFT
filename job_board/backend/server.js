1f5734870b16bc9df83f551166a708d9fb8427d2
sisekelongcobo <2433205@students.wits.ac.za>
2024-09-24 03:00:23 +0200

    fix sever

--- job_board/backend/server.js
+++ job_board/backend/server.js
@@ -1,6 +1,7 @@
 import process from 'process';
 import app from './app.js';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.MYSQL_PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
