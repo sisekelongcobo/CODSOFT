1edac5dac669e9c49bae732ac2bdc0d75ee279d1
sisekelongcobo <2433205@students.wits.ac.za>
2024-09-25 17:18:02 +0200

    Added the homepage and the joblisting page

--- job_board/frontend/src/components/Layout.tsx
+++ job_board/frontend/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Grid } from '@mui/material';
+import { Box } from '@mui/material';
 import { NavBar } from './NavBar';
 import { Footer } from './Footer';
 
